ea2038e5353537b647772c45b1e6a7b778e11975
Simplificatedd <simplificatedd@gmail.com>
2025-09-19 01:28:13 +0800

    Enhancement 1

--- Mini project/RockPaperScissors_Enhancements.py
+++ Mini project/RockPaperScissors_Enhancements.py
@@ -1,19 +1,57 @@
+# Enhancement 1: Refactor the code to use functions
+
+#region ##### import #####
 import random
+#endregion
 
+#region ##### Variables #####
 PossibleMoves = ['rock', 'paper', 'scissors']
+#endregion
 
-playerMove = input("Your move? (Rock/Paper/Scissors): ").lower()
-computerMove = random.choice(PossibleMoves)
+#region ##### Functions #####
+def get_player_move():
+    playerMove = input("Your move? (Rock/Paper/Scissors): ").lower()
+    return playerMove
 
-if playerMove in PossibleMoves:
+def validate_player_move(playerMove):
+    if playerMove in PossibleMoves:
+        return True
+    else:
+        return False
+
+def get_computer_move():
+    computerMove = random.choice(PossibleMoves)
+    return computerMove
+
+def display_moves(playerMove, computerMove):
     print(f"You chose {playerMove}.")
     print(f"CPU chose {computerMove}.")
-    
+
+def decide_outcome(playerMove, computerMove):
     if playerMove == computerMove:
-        print("It is a draw.\n")
+        return 1
     elif (playerMove == "rock" and computerMove == "scissors") or (playerMove == "paper" and computerMove == "rock") or (playerMove == "scissors" and computerMove == "paper"):
-        print("You won!\n")
+        return 2
     else:
+        return 3
+
+def show_conclusion(outcome):
+    if outcome == 1:
+        print("It is a draw.\n")
+    elif outcome == 2:
+        print("You won!\n")
+    elif outcome == 3:
         print("You lost!\n")
+#endregion
+
+#region ##### Main code #####
+playerMove = get_player_move()
+if validate_player_move(playerMove):
+    computerMove = get_computer_move()
+    display_moves(playerMove, computerMove)
+    outcome = decide_outcome(playerMove, computerMove)
+    show_conclusion(outcome)
+    
 else:
     print("Invalid move. Please enter 'Rock', 'Paper', or 'Scissors'.")
+#endregion
